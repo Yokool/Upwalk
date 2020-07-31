a750a26047b3a3eaf561f718d2f98c798e583ab0
Yokool <letsgamecze@gmail.com>
2020-07-31 20:11:20 +0200

    oh?

---
+++ Assets/Resources/Scripts/GridComponents/Moveable/CallOwnTriggerOnArrival.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(GridObject))]
+[RequireComponent(typeof(GridObjectTrigger))]
+public class CallOwnTriggerOnArrival : MonoBehaviour, IObjectArrivalCallback
+{
+    public void ObjectArrived()
+    {
+        GridObject thisGridObject = GetComponent<GridObject>();
+        GridObjectTrigger thisTrigger = GetComponent<GridObjectTrigger>();
+        foreach(GridObject obj in GameGrid.INSTANCE.GridObjectsAtObjectFiltered(thisGridObject))
+        {
+            thisTrigger.NotifyTrigger(obj);
+        }
+        
+    }
+}
---
+++ Assets/Resources/Scripts/Utilities/GridObjectUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GridObjectUtility
+{
+
+    public static int Distance(GridObject gridObjectOne, GridObject gridObjectTwo)
+    {
+
+        return Math.Max(gridObjectTwo.X - gridObjectOne.X, gridObjectOne.Y - gridObjectTwo.Y);
+
+    }
+
+}
